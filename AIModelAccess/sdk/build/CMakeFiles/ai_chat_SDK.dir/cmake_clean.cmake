484523f62cab98697e27e094142bdd5a1774a550
孟新大人 <2831850183@qq.com>
2026-04-29 15:27:04 +0800

    增加了cmake正式的构建包

---
+++ AIModelAccess/sdk/build/CMakeFiles/ai_chat_SDK.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_chat_SDK.dir/src/ChatGPTProvider.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/ChatSDK.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/DataManager.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/DeepSeekProvider.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/GeminiProvider.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/LLMManager.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/OllamaProvider.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/SessionManager.cpp.o"
+  "CMakeFiles/ai_chat_SDK.dir/src/util/myLog.cpp.o"
+  "libai_chat_SDK.a"
+  "libai_chat_SDK.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_chat_SDK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
